ec9c0a4003cbbc7038795308c3e0c2c0cd6c8919
ttuan9909 <ttanhtuan2003@gmail.com>
2025-02-25 21:56:43 +0700

    them CRUD Project

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const volunteerRoutes = require('./volunteerRoutes');
+const projectRoutes = require('./projectRoutes');
 
 const router = express.Router();
 
 // Định tuyến các route
 router.use('/volunteers', volunteerRoutes);
+router.use('/projects', projectRoutes);
+
 
 module.exports = router;
