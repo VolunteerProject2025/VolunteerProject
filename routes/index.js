355bc934839a81c3ce4f9b8f439b60ae575b5bc0
KietNguyen2301 <Kietde160233@gmail.com>
2025-02-26 08:47:36 +0700

    Back-end CRUD Post

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const volunteerRoutes = require('./volunteerRoutes');
+const postRoutes = require('./postRoutes');
+const scheduleRoutes = require('./scheduleRoutes');
+
 
 const router = express.Router();
 
 // Định tuyến các route
 router.use('/volunteers', volunteerRoutes);
+router.use('/post', postRoutes);
+router.use('/schedules', scheduleRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const routes = require('./routes');
 const connectDB = require('./config/db');
 
@@ -11,6 +12,7 @@ connectDB();
 
 // Middleware
 app.use(bodyParser.json());
+app.use(cors());
 
 // Routes
 app.use('/', routes);
